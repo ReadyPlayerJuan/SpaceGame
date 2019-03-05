2da9d94cc5bdc5a05d825719f10a6a82e80e4446
Nick Seel <nick.seel@gmail.com>
2019-03-04 23:57:47 -0600

    left out of prev commit

--- src/main/views/GameView.java
+++ src/main/views/GameView.java
@@ -1,21 +1,30 @@
 package main.views;
 
 import main.game.boards.Board;
+import main.game.enums.Team;
 import main.game.ships.Ship;
+import main.game.ships.TestShip;
+import main.input.InputCode;
 import org.lwjgl.opengl.GL11;
 
 public class GameView extends View {
-    public Board currentBoard;
+    private Board currentBoard;
     private Ship playerShip; //maybe make this a blueprint, and then create a new player ship for each board
 
     public GameView(int width, int height) {
         super(width, height);
 
         currentBoard = new Board(this, 4);
+        playerShip = new TestShip(this, Team.PLAYER);
     }
 
     public void updateSelf(double delta) {
         currentBoard.update(delta);
+        playerShip.update(currentBoard, delta);
+    }
+
+    public void processInput(InputCode code) {
+        System.out.println("GAMEVIEW RECIEVED CODE " + code);
     }
 
     public void drawSelf() {
@@ -24,6 +33,7 @@ public class GameView extends View {
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
         currentBoard.draw();
+        playerShip.draw(currentBoard, width, height);
         mainFrameBuffer.unbindFrameBuffer();
     }
 }
